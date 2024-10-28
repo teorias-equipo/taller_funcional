7092f666d20eb527426194e3f1fc364c72fbd4b6
Carlos Rojas <carlosrojasmartinez19@gmail.com>
2024-10-28 07:51:51 -0500

    Primer Manejo de Archivo

---
+++ src/main/java/edu/javeriana/taller/TallerApplication.java
@@ -0,0 +1,13 @@
+package edu.javeriana.taller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/javeriana/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.javeriana.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
